5897723046f04e5c69efda92aaac0a672ec71708
karthik74 <106848632+Raghunandan1157@users.noreply.github.com>
2025-11-04 19:44:59 +0530

    1

---
+++ NMSPL LOCATION/location_website/script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickBtn").addEventListener("click", function() {
+  const msg = document.getElementById("message");
+  msg.textContent = "🚀 You clicked the button! Welcome to your live website!";
+});
